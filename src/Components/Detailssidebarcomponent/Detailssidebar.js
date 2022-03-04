abd3062964d7d532345ceee67d466129bc34d43c
Pranali777 <shirsat7pranali@gmail.com>
2022-03-04 14:05:39 +0100

    Details sidebar component added

---
+++ src/Components/Detailssidebarcomponent/Detailssidebar.js
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import Address from '../Services/Address';
+import Payment from '../Services/Payment';
+import DateTime from '../Services/DateTime';
+import Details from "../Services/Details";
+import './Detailssidebar.css';
+
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+
+const Detailssidebar = () =>{
+    return(
+       <div className="left_div">
+       <nav className="Details_nav">
+          
+       
+       </nav>
+
+      
+       </div>
+    )
+}
+
+
+export default Detailssidebar;
