cc3fb52aad46561ff0009f71d9f7f8956eed6927
Pranali777 <shirsat7pranali@gmail.com>
2022-03-04 14:00:10 +0100

    text area component added

---
+++ src/Components/textareacomponent/Textareacomponent.js
@@ -0,0 +1,10 @@
+import "./Textareacomponent.css"
+
+
+const Textareacomponent = (props)=>{
+    return(
+        <textarea placeholder={props.value}/>
+    )
+}
+
+export default Textareacomponent;
