b22482a90e21ce162ce47a6161fc0514ae541ea1
Pranali777 <shirsat7pranali@gmail.com>
2022-03-04 13:53:45 +0100

    Text Line Component added

---
+++ src/Components/textlinecomponent/textlinecomponent.js
@@ -0,0 +1,9 @@
+import './textlinecomponent.css';
+
+const Textlinecomponent =() => {
+return(
+    <input type="text" id="Whatstobedone_input" class="text-line" ></input>
+)
+}
+
+export default Textlinecomponent;
