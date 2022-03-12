994d06b22c84c87eab544df0c088991611f00e39
Pranali777 <shirsat7pranali@gmail.com>
2022-03-12 19:44:34 +0100

    header added with icons and css

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,39 +1,38 @@
 
 // import handshake from './images/handshake.png';
 import './Header.css'
-import { NavLink, Route, Switch } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Detailssidebar from '../Detailssidebarcomponent/Detailssidebar';
 import Home from '../Home/Home';
 
+import { FaHandshake } from "react-icons/fa";
+import { MdOutlineAccountCircle, MdOutlineHome } from "react-icons/md";
 
-const Header=()=>{
 
-    return(
+
+const Header = () => {
+
+    return (
         <div className="Header_div">
             <div className="image_div">
-           {/* <img src={handshake}></img> */}
-        
-         
-
+                <FaHandshake className='Handshake_icon' />
             </div>
 
             <div className="Heading_div">
-            <nav>
-         <NavLink to="/details" exact>Card Componenet</NavLink>
-        <NavLink to="/" exact>Home</NavLink> 
-         </nav>
-         <Switch>
-       <Route path="/details" component = {Detailssidebar}/>
-      <Route path ="/" exact component={Home}/>
-    </Switch>
-            </div>
+            <h1>HelpingHands</h1>
+           </div>
 
             <div className="Login_div">
-                
+                 <nav>
+                    <NavLink to="/Home" exact><MdOutlineHome className='home_icon' /></NavLink>
+                </nav>
+
             </div>
 
             <div className="Profile_div">
-
+           
+            <MdOutlineAccountCircle className='account_icon' />             
+            <button id="btn_profile" accept='image' name="file" className='profilebtn' > </button>
             </div>
 
 
